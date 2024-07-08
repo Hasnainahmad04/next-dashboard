81d59ac70927fe3b47e8c046017d4c83e5dd234d
Hasnain Ahmad <hasnainahmadkhan04@gmail.com>
2024-07-08 18:28:04 +0500

    ref: removed unused imports

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,5 @@
 import { fetchFilteredCustomers } from '@/lib/data';
 import CustomersTable from '@/ui/customers/table';
-import { useParams, useRouter } from 'next/navigation';
 
 const Page = async ({ searchParams }: { searchParams: { query?: string } }) => {
   const customers = await fetchFilteredCustomers(searchParams?.query || '');
