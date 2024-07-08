c3d9b51b931d5b651582792b92811ab38562a2ec
Hasnain Ahmad <hasnainahmadkhan04@gmail.com>
2024-07-08 18:26:52 +0500

    feat: added customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
-const Page = () => {
-  return <span>Customers</span>;
+import { fetchFilteredCustomers } from '@/lib/data';
+import CustomersTable from '@/ui/customers/table';
+import { useParams, useRouter } from 'next/navigation';
+
+const Page = async ({ searchParams }: { searchParams: { query?: string } }) => {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+  return <CustomersTable customers={customers} />;
 };
 
 export default Page;
